93ca577323ab1abc3625e9f5bf31062c6028e306
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-03 14:08:51 -0500

    Added request.getRequestDispatcher to doGet method.

--- src/main/java/PickColorServlet.java
+++ src/main/java/PickColorServlet.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class PickColorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("/pickcolor.jsp").forward(request, response);
     }
 
     @Override
