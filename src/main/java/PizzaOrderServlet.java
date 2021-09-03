880c08ae1d5727a3e8e0d5da393169c6607bd27a
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-03 10:41:58 -0500

    Completed PizzaOrderServlet doPost method, getting parameters of pizza order and printing to the console

--- src/main/java/PizzaOrderServlet.java
+++ src/main/java/PizzaOrderServlet.java
@@ -3,7 +3,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "PizzaOrderServlet", value = "/PizzaOrderServlet", urlPatterns = "/pizza-order")
+@WebServlet(name = "PizzaOrderServlet", urlPatterns = "/pizza-order")
 public class PizzaOrderServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -12,6 +12,13 @@ public class PizzaOrderServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String crust = request.getParameter("crust");
+        String size = request.getParameter("size");
+        String sauce = request.getParameter("sauce");
+        String[] toppings = request.getParameterValues("toppings");
+        String toppingsString = String.join(", ", toppings);
+        String address = request.getParameter("address");
 
+        System.out.printf("crust: %s%nsize: %s%nsauce: %s%ntoppings: %s%naddress: %s%n", crust, size, sauce, toppingsString, address);
     }
 }
