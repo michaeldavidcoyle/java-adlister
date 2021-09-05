6a517bdc028a281bebb30239c0b248ac219f9041
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-04 23:48:25 -0500

    Started BeanTest.

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,8 @@
+public class BeanTest {
+
+    public static void main(String[] args) {
+        Album thriller = new Album();
+        Album help = new Album();
+        Album morningGlory = new Album();
+    }
+}
