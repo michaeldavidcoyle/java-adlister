d1eee738fb11e7b4f4be47edb1d61401a7f37108
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-08 11:05:49 -0500

    Redirecting logged in user who visits /login to /profile.

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,7 +8,12 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+        } else {
+            request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
+        }
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
