7b5353bbf46c48c934e7a32b74b6d8a527736701
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-07 21:15:23 -0500

    Updated LoginServlet and ViewProfileServlet to redirect unauthorized users back to login page.

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/profile.jsp").forward(request, response);
+        HttpSession currentSession = request.getSession();
+        String authorizedUser = (String) currentSession.getAttribute("user");
+
+        if (authorizedUser != null) {
+            if (authorizedUser.equals("admin")) {
+                request.getRequestDispatcher("WEB-INF/profile.jsp").forward(request, response);
+            }
+        }
+
+        response.sendRedirect("/login");
     }
 }
