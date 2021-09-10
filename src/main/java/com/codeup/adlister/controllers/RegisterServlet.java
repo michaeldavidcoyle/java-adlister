afabe7fd404f5397aad2e7558b7c008866f831ac
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-10 16:54:24 -0500

    Updated User model to hash password before setting it and updated RegisterServlet to use setPassword method rather than constructor.

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -34,7 +34,12 @@ public class RegisterServlet extends HttpServlet {
         }
 
         // create and save a new user
-        User user = new User(username, email, password);
+        User user = new User();
+
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+
         DaoFactory.getUsersDao().insert(user);
         response.sendRedirect("/login");
     }
