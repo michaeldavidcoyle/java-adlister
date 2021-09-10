e118dd6daaeafe7a89d0352aefb8fab607009923
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-10 12:04:08 -0500

    Added appropriate fields and method to DaoFactory to return an instance of users dao.

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -4,6 +4,7 @@ import com.codeup.adlister.Config;
 
 public class DaoFactory {
     private static Ads adsDao;
+    private static Users usersDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -11,5 +12,13 @@ public class DaoFactory {
             adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
+    }
+
+    public static Users getUsersDao() {
+        if (usersDao == null) {
+            usersDao = new MySQLUsersDao(config);
+        }
+
+        return usersDao;
     }
 }
